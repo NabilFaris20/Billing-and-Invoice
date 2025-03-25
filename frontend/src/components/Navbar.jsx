13a713b26e5b77cded54535b7ee509ec6b714e07
Nabil <nabilfaris50@gmail.com>
2025-03-25 16:52:39 +1000

    small navbar change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             <Link to="/login" className="mr-4">Login</Link>
             <Link
               to="/register"
-              className="bg-black-500 px-4 py-2 rounded hover:bg-red-700"
+              className="bg-green-500 px-4 py-2 rounded hover:bg-green-700"
             >
               Register
             </Link>
